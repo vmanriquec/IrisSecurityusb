b5ae8ac20f5ae5ad8a0058e75ce8636149f3b02a
Programmer 7 - Jhonatan <programmer7@apolomultimedia.com>
2016-06-20 14:39:55 -0500

    first commit

---
+++ app/src/main/java/com/apolomultimedia/irissecurity/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.apolomultimedia.irissecurity;
+
+import android.content.Context;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(base));
+    }
+
+}
---
+++ app/src/main/java/com/apolomultimedia/irissecurity/util/Constantes.java
@@ -0,0 +1,10 @@
+package com.apolomultimedia.irissecurity.util;
+
+/**
+ * Created by developer on 20/06/2016.
+ */
+public class Constantes {
+
+    
+
+}
